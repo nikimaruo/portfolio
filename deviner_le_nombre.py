7e8d6c9e4889ffe20422a1db04f6554fc1a0b0dd
nikimaruo <sybaps009@gmail.com>
2025-06-06 20:27:13 +0000

    Create deviner_le_nombre.py

---
+++ deviner_le_nombre.py
@@ -0,0 +1,4 @@
+temp_celsius = float(input("Entre la temperature en celsius : "))
+temp_fahrenheit = (temp_celsius * 1.8) + 32     #°F =( °C x 1,8 ) + 32
+print(f"La température donnée en celsius est : {temp_celsius} °C")
+print(f"La temperature en fahrenheit est : {temp_fahrenheit} °C")
